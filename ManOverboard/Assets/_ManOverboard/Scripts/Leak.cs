f4c650472ddfaf020160bc7abd040befd734d6e6
Arrow-Desktop <arrow_m13@hotmail.com>
2018-08-07 19:43:06 -0400

    Created the leak script for managing the item

---
+++ ManOverboard/Assets/_ManOverboard/Scripts/Leak.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using ZeroProgress.Common;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Joint2D))]
+public class Leak : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip("The joint that connects this leak to the boat")]
+    private FixedJoint2D boatConnection;
+
+    [SerializeField]
+    [Tooltip("The rigidbody that represents the weight caused by the leak")]
+    private Rigidbody2D myRigidbody;
+
+    private void Awake()
+    {
+        boatConnection = this.GetComponentIfNull(boatConnection);
+
+        myRigidbody = this.GetComponentIfNull(myRigidbody);
+    }
+
+    public void AttachLeak(Rigidbody2D BoatRigid)
+    {
+        boatConnection.connectedBody = BoatRigid;
+    }
+
+}
