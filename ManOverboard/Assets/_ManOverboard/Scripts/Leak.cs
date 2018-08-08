990a1f928b76c9e4238967ab3152b10fc7428ba9
Arrow-Desktop <arrow_m13@hotmail.com>
2018-08-07 20:42:51 -0400

    Set up the fill coroutine which adds weight to the boat on a set interval

--- ManOverboard/Assets/_ManOverboard/Scripts/Leak.cs
+++ ManOverboard/Assets/_ManOverboard/Scripts/Leak.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using ZeroProgress.Common;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(Joint2D))]
@@ -12,6 +13,18 @@ public class Leak : MonoBehaviour {
     [Tooltip("The rigidbody that represents the weight caused by the leak")]
     private Rigidbody2D myRigidbody;
 
+    [SerializeField]
+    [Tooltip("How much weight to apply to the leak")]
+    private float fillRate = 0.001f;
+
+    [SerializeField]
+    [Tooltip("How often to apply the fill rate")]
+    private float fillInterval = 0.5f;
+
+    private bool isAttachedToBoat = false;
+
+    private Coroutine fillCoroutine = null;
+    
     private void Awake()
     {
         boatConnection = this.GetComponentIfNull(boatConnection);
@@ -19,9 +32,34 @@ public class Leak : MonoBehaviour {
         myRigidbody = this.GetComponentIfNull(myRigidbody);
     }
 
+    private void OnEnable()
+    {
+        fillCoroutine = StartCoroutine(LeakFill());
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(fillCoroutine);
+        fillCoroutine = null;
+    }
+
     public void AttachLeak(Rigidbody2D BoatRigid)
     {
         boatConnection.connectedBody = BoatRigid;
+        isAttachedToBoat = true;
     }
+
+    private IEnumerator LeakFill()
+    {
+        while (true)
+        {
+            if (isAttachedToBoat)
+                myRigidbody.mass += fillRate;
+
+            yield return new WaitForSeconds(fillInterval);
+        }
+    }
+
+
 
 }
