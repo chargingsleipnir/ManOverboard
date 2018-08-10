9547fc07c5676e6fc68a7d3bab994fdb66359c5e
Odin von Doom <devinodin@gmail.com>
2018-08-09 20:37:51 -0400

    Just added some characters and basic GUI info

---
+++ ManOverboard/Assets/_ManOverboard/Scripts/Dinghy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dinghy : MonoBehaviour {
+
+    public int buoyancy = 350;
+    public GameObject [] passengers;
+    public int loadWeight;
+
+    public UnityEngine.UI.Text buoyancyDisp;
+    public UnityEngine.UI.Text loadWeightDisp;
+
+    // Use this for initialization
+    void Start () {
+        buoyancyDisp.text = buoyancy.ToString();
+        InvokeRepeating("Sink", 2.0f, 1.0f);
+
+        for(var i = 0; i < passengers.Length; i++) {
+            loadWeight += passengers[i].GetComponent<Passenger>().weight;
+        }
+
+        loadWeightDisp.text = loadWeight.ToString();
+    }
+
+    void Sink() {
+        buoyancy--;
+        buoyancyDisp.text = buoyancy.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ ManOverboard/Assets/_ManOverboard/Scripts/Passenger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passenger : MonoBehaviour {
+
+    public int weight = 100;
+    public bool mouseHeld = false;
+
+    public UnityEngine.UI.Text weightDisp;
+
+    // Use this for initialization
+    void Start () {
+        weightDisp.text = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnMouseDown()
+    {
+        mouseHeld = true;
+    }
+
+    private void OnMouseUp()
+    {
+        mouseHeld = false;
+        weightDisp.text = "";
+    }
+
+    private void OnMouseExit()
+    {
+        mouseHeld = false;
+        weightDisp.text = "";
+    }
+
+    private void OnMouseOver()
+    {
+        if (mouseHeld)
+            weightDisp.text = weight.ToString();
+    }
+}
